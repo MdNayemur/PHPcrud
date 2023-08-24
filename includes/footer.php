91b6855be07d6f462f88925ddb97f438a77c2b51
Nayemur Rahman <97352496+MdNayemur@users.noreply.github.com>
2023-08-24 12:35:54 +0600

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<script src="js/jquery.3.6.0.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+<script src="js/all.min.js"></script>
+<script src="//cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>  
+    <script>
+      $(document).ready( function () {
+    $('#myTable').DataTable();
+    } );
+    </script>  
+</body>
+</html>
